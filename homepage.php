285faa2244c37939487f5506f1c4e0c659ad55ec
Johannes Beier <johabei@gmx.de>
2024-06-13 09:47:03 +0200

    Dummy Login Area added

---
+++ homepage.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LoginBereich</title>
+</head>
+<body>
+    <?php
+        session_start();
+        if(isset($_SESSION['username'])){
+            echo "<h1>Willkommen ".$_SESSION['first_name'].$_SESSION['last_name']."</h1>";
+        } else {
+            header("Location: index.php");
+        }
+    ?> 
+    <h1></h1>
+</body>
+</html>
